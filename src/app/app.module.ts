b8dd40bd1e3711da6a07bf214b61d7ccbb5bcd0b
thanglv <levietthang1997@gmail.com>
2022-05-08 23:41:44 +0700

    ngzoro + tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {en_US, NZ_I18N} from 'ng-zorro-antd/i18n';
 import {registerLocaleData} from '@angular/common';
 import en from '@angular/common/locales/en';
 import {FormsModule} from '@angular/forms';
@@ -10,13 +9,8 @@ import {HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {IconsProviderModule} from './icons-provider.module';
-import {NzLayoutModule} from 'ng-zorro-antd/layout';
-import {NzMenuModule} from 'ng-zorro-antd/menu';
 import {LoginComponent} from './pages/auth/login/login.component';
 import {AdminComponent} from './pages/admin/admin.component';
-import {NzInputModule} from 'ng-zorro-antd/input';
-import {NzButtonModule} from 'ng-zorro-antd/button';
-import {NzFormModule} from "ng-zorro-antd/form";
 import {DemoNgZorroAntdModule} from "./ng-zorro-antd.module";
 
 registerLocaleData(en);
@@ -34,14 +28,8 @@ registerLocaleData(en);
     BrowserAnimationsModule,
     AppRoutingModule,
     IconsProviderModule,
-    NzLayoutModule,
-    NzMenuModule,
-    NzInputModule,
-    NzButtonModule,
-    NzFormModule,
     DemoNgZorroAntdModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
