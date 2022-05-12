731e616d6434efd70e669069f06e2abdeca8c254
thanglv <levietthang1997@gmail.com>
2022-05-13 00:13:50 +0700

    Thêm form

--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -7,9 +7,11 @@ import {NzMenuModule} from "ng-zorro-antd/menu";
 import {IconsProviderModule} from "../../icons-provider.module";
 import {UserManagementComponent} from './user-management/user-management.component';
 import {DemoNgZorroAntdModule} from "../../ng-zorro-antd.module";
+import {ReactiveFormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
-  imports: [AdminRoutingModule, NzLayoutModule, NzMenuModule, IconsProviderModule, DemoNgZorroAntdModule],
+  imports: [AdminRoutingModule, NzLayoutModule, NzMenuModule, IconsProviderModule, DemoNgZorroAntdModule, ReactiveFormsModule, CommonModule],
   declarations: [AdminComponent, UserManagementComponent],
   exports: [AdminComponent]
 })
--- src/app/pages/admin/user-management/user-management.component.ts
+++ src/app/pages/admin/user-management/user-management.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import {NzModalService} from "ng-zorro-antd/modal";
+import {Data} from "@angular/router";
 
 @Component({
   selector: 'app-user-management',
@@ -6,11 +9,105 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./user-management.component.css']
 })
 export class UserManagementComponent implements OnInit {
-  isCollapsed = true;
+  validateForm!: FormGroup;
+  controlArray: Array<{ index: number; show: boolean }> = [];
+  isCollapse = true;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private modalService: NzModalService) {}
+
+  toggleCollapse(): void {
+    this.isCollapse = !this.isCollapse;
+    this.controlArray.forEach((c, index) => {
+      c.show = this.isCollapse ? index < 3 : true;
+    });
+  }
+
+  resetForm(): void {
+    this.validateForm.reset();
+  }
 
   ngOnInit(): void {
+    this.validateForm = this.fb.group({});
+    for (let i = 0; i < 6; i++) {
+      this.controlArray.push({ index: i, show: i < 3 });
+      this.validateForm.addControl(`field${i}`, new FormControl());
+    }
+
+    this.listOfData = new Array(100).fill(0).map((_, index) => ({
+      id: index,
+      name: `Edward King ${index}`,
+      age: 32,
+      address: `London, Park Lane no. ${index}`,
+      disabled: index % 2 === 0
+    }));
+  }
+
+  isVisible = false;
+  isConfirmLoading = false;
+
+  showModal1(): void {
+    this.isVisible = true;
+  }
+
+  handleOk(): void {
+    this.isConfirmLoading = true;
+    setTimeout(() => {
+      this.isVisible = false;
+      this.isConfirmLoading = false;
+    }, 3000);
+  }
+
+  handleCancel(): void {
+    this.isVisible = false;
+  }
+
+  checked = false;
+  loading = false;
+  indeterminate = false;
+  listOfData: readonly Data[] = [];
+  listOfCurrentPageData: readonly Data[] = [];
+  setOfCheckedId = new Set<number>();
+
+  updateCheckedSet(id: number, checked: boolean): void {
+    if (checked) {
+      this.setOfCheckedId.add(id);
+    } else {
+      this.setOfCheckedId.delete(id);
+    }
+  }
+
+  onCurrentPageDataChange(listOfCurrentPageData: readonly Data[]): void {
+    this.listOfCurrentPageData = listOfCurrentPageData;
+    this.refreshCheckedStatus();
+  }
+
+  refreshCheckedStatus(): void {
+    const listOfEnabledData = this.listOfCurrentPageData.filter(({ disabled }) => !disabled);
+    this.checked = listOfEnabledData.every(({ id }) => this.setOfCheckedId.has(id));
+    this.indeterminate = listOfEnabledData.some(({ id }) => this.setOfCheckedId.has(id)) && !this.checked;
+  }
+
+  onItemChecked(id: number, checked: boolean): void {
+    this.updateCheckedSet(id, checked);
+    this.refreshCheckedStatus();
+  }
+
+  onAllChecked(checked: boolean): void {
+    this.listOfCurrentPageData
+      .filter(({ disabled }) => !disabled)
+      .forEach(({ id }) => this.updateCheckedSet(id, checked));
+    this.refreshCheckedStatus();
+  }
+
+  sendRequest(): void {
+    this.loading = true;
+    const requestData = this.listOfData.filter(data => this.setOfCheckedId.has(data['id']));
+    console.log(requestData);
+    setTimeout(() => {
+      this.setOfCheckedId.clear();
+      this.refreshCheckedStatus();
+      this.loading = false;
+    }, 1000);
   }
 
 }
