c05ff2da09f3303a73dfc83fda8d7ba7557aca30
thanglv <levietthang1997@gmail.com>
2022-05-17 17:35:50 +0700

    Hoàn tất crud form prod

--- src/app/constants/GlobalConstants.ts
+++ src/app/constants/GlobalConstants.ts
@@ -2,6 +2,6 @@ import {Injectable} from "@angular/core";
 
 @Injectable()
 export class GlobalConstants {
-  public static IS_PROD: boolean = false;
+  public static IS_PROD: boolean = true;
   public static readonly API_ENDPOINT: string = GlobalConstants.IS_PROD ? ' https://api.kttk.xyz/api/v1/' : 'http://localhost:9999/api/v1/';
 }
